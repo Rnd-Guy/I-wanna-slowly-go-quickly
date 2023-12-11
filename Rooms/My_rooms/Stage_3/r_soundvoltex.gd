fb44975b5b573fd05c97fcf9ebd6597300d85848
Rnd-Guy <kevinngo@hotmail.co.uk>
2023-12-11 10:46:35 +0000

    add initial attempt at final rhythm level

--- Rooms/My_rooms/Stage_3/r_soundvoltex.gd
+++ Rooms/My_rooms/Stage_3/r_soundvoltex.gd
@@ -1,13 +1,12 @@
 extends Node2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	%objPlayer.h_speed = 6
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$Room_related/objCameraFixedNoSmoothing.rotation += delta
 	pass
 
