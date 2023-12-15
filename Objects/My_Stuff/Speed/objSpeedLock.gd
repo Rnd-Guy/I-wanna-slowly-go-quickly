2a38cf52bee45354f42c2858ea0d308647d859bb
Rnd-Guy <kevinngo@hotmail.co.uk>
2023-12-15 20:22:35 +0000

    update lock graphics

--- Objects/My_Stuff/Speed/objSpeedLock.gd
+++ Objects/My_Stuff/Speed/objSpeedLock.gd
@@ -22,6 +22,17 @@ var is_locked: bool = false
 ## For direct equality leniency, how much leeway to give
 @export var leniency = 0.01
 
+@onready var sprites = {
+	"perma_open": $perma/open,
+	"perma_closed": $perma/closed,
+	"temp_open": $temp/open,
+	"temp_closed": $temp/closed,
+	"breakable_open": $breakable/open,
+	"breakable_closed": $breakable/closed,
+}
+var closed = null
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	GLOBAL_INSTANCES.player_speed_changed.connect(on_player_speed_change)
@@ -48,26 +59,28 @@ func _ready():
 	update_text_scaling()
 	if get_tree().get_first_node_in_group("Player"):
 		on_player_speed_change(get_tree().get_first_node_in_group("Player").h_speed)
+	
+	handle_sprites()
 
 func on_player_speed_change(newSpeed):
 	var current_value = $StaticBody2D.get_collision_layer_value(2)
-	var new_value: bool
 	if ((above && newSpeed > speed) ||
 			(below && newSpeed < speed) ||
 			(equal && is_equal(newSpeed, speed))):
-		new_value = false
-		
+		closed = false
 	else:
-		new_value = true
+		closed = true
 	
-	if current_value != new_value && !is_locked:
-		$StaticBody2D.set_collision_layer_value(2, new_value)
-		if perma_lock:
+	#if current_value != closed && !is_locked:
+	if !is_locked:
+		$StaticBody2D.set_collision_layer_value(2, closed)
+		if perma_lock && closed:
 			is_locked = true
-		if new_value:
-			modulate.a = 1
-		else:
-			modulate.a = 0.5
+#		if new_value:
+#			modulate.a = 1
+#		else:
+#			modulate.a = 0.5
+	handle_sprites()
 
 func is_equal(speeda, speedb, len=leniency):
 	#print(str(speeda) + " " + str(speedb) + " " + str(len) + " " + str(abs(speeda - speedb) <= leniency))
@@ -85,3 +98,38 @@ func update_text_scaling():
 		$Control.scale.x = (yscale/xscale)
 	else:
 		$Control.scale.y *= (xscale/yscale)
+
+func handle_sprites():
+	if perma_lock:
+		$perma.set_visible(true)
+		$temp.set_visible(false)
+		$breakable.set_visible(false)
+		if is_locked:
+			turn_off_sprites_except_this_one("perma_closed")
+		else:
+			turn_off_sprites_except_this_one("perma_open")
+	elif one_use:
+		$perma.set_visible(false)
+		$breakable.set_visible(true)
+		$temp.set_visible(false)
+		if closed:
+			turn_off_sprites_except_this_one("breakable_closed")
+		else:
+			turn_off_sprites_except_this_one("breakable_open")
+	else:
+		$perma.set_visible(false)
+		$breakable.set_visible(false)
+		$temp.set_visible(true)
+		if closed:
+			turn_off_sprites_except_this_one("temp_closed")
+		else:
+			turn_off_sprites_except_this_one("temp_open")
+		
+		
+		
+func turn_off_sprites_except_this_one(index):
+	for i in sprites:
+		if i == index:
+			sprites[i].set_visible(true)
+		else:
+			sprites[i].set_visible(false)
