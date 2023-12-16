40619b87922d6c52fd098e98f70d3b7541139606
Rnd-Guy <kevinngo@hotmail.co.uk>
2023-12-16 20:33:32 +0000

    create almost all colorful stage mech

--- Rooms/My_rooms/Stage_3/Note.gd
+++ Rooms/My_rooms/Stage_3/Note.gd
@@ -1,7 +1,52 @@
 extends Node2D
 
+signal collided
+
+var speed = 300
+var bpm = 220
+var beat = 1# which beat this note should reach the middle
+var x = 100
+var target_y = 490
+var offset = 0
+var flip = false
+var time_offset = 0
+var global_offset = 0
+
+var seconds_per_beat # 60 / bpm
+var time_to_reach_middle
+var music_time
+
+func _ready():
+	seconds_per_beat = 60.0/bpm
+	time_to_reach_middle = (beat * seconds_per_beat) + time_offset + global_offset
+	resync_rhythm_position()
+
+func resync_rhythm_position():
+	global_offset = GLOBAL_SETTINGS.MUSIC_OFFSET/1000.0
+	time_to_reach_middle = (beat * seconds_per_beat) + time_offset + global_offset
+	music_time = GLOBAL_MUSIC.get_playback_position() + AudioServer.get_time_since_last_mix() - AudioServer.get_output_latency()
+	var distance = (time_to_reach_middle - music_time) * speed
+	set_global_position(Vector2(x,target_y + offset - distance))
+
+func _on_timer_timeout():
+	resync_rhythm_position()
+	pass # Replace with function body.
+
+func on_the_beat():
+	if fmod(music_time, seconds_per_beat) < 0.2:
+		var mod = 1.5 - (fmod(music_time, seconds_per_beat) * 2.5)
+		$Sprite2D.modulate = Color(mod, mod, mod)
+	else:
+		$Sprite2D.modulate = Color(1,1,1)
+
 func _process(delta):
 	$Sprite.get_material().set_shader_parameter("screen_position", global_position)
 
 func _physics_process(delta):
-	position.y += 100*delta
+	position.y += speed*delta
+
+
+
+func _on_area_2d_body_entered(body):
+	collided.emit()
+	queue_free()
--- Rooms/My_rooms/Stage_3/r_soundvoltex.gd
+++ Rooms/My_rooms/Stage_3/r_soundvoltex.gd
@@ -2,24 +2,213 @@ extends Node2D
 
 var note = preload("res://Objects/My_Stuff/Rhythm/objColourfulNote.tscn")
 
+
 var x_positions = [
 	208, 280, 352, 416, 488, 560
 ]
 var y_target = 490
 
+var target_score = 11600 # max possible without z, max is 12400
+
 var notes = [
-	[1, 234]
+	[2, 3],
+	[4,1],
+	
+	[7,1],
+	[8,2],
+	[9,2],
+	[9.5,2],
+	[10.5,3],
+	[12,3],
+	
+	[15,3],
+	[16,4],
+	[17,4],
+	[17.5,4],
+	[18.5,4],
+	[19.5,5],
+	[20.5,3],
+	[21,3],
+	
+	[23,3],
+	[24,3],
+	[26,5],
+	[28,4],
+	[29,5],
+	[30,4],
+	[31,3],
+	[32,4],
+	
+	[34,6],
+	[36,1],
+	
+	[39,3],
+	[40,4],
+	[41,4],
+	[41.5,4],
+	[42.5,5],
+	[44,5],
+	
+	[47,2],
+	[48,4],
+	[49,4],
+	[49.5,4],
+	[50.5,4],
+	[51.5,5],
+	[52.5,3],
+	
+	[55,3],
+	[56,3],
+	[58,6],
+	[60,3],
+	[61,4],
+	[62,3],
+	[63,2],
+	[64,3],
+	
+	[66,6],
+	[68,1],
+	
+	[71,1],
+	[72,6],
+	[73,6],
+	[73.5,6],
+	[74.5,1],
+	[76,6],
+	
+	[79,4],
+	[80,5],
+	[81,5],
+	[81.5,5],
+	[82.5,5],
+	[83.5,6],
+	[84.5,2],
+	[85,2],
+	
+	[87,2],
+	[88,2],
+	[90,6],
+	[92,2],
+	[93,3],
+	[94,2],
+	[95,1],
+	[96,2],
+	
+	[98,5],
+	[100,2],
+	
+	[103,2],
+	[104,4],
+	[105,3],
+	[105.5,3],
+	[106.5,2],
+	[108,4],
+	
+	[111,1],
+	[112,3],
+	[113,3],
+	[113.5,3],
+	[114.5,3],
+	[115.5,4],
+	[116.5,2],
+	
+	[119,2],
+	[120,2],
+	[122,5],
+	[124,4],
+	[125,5],
+	[126,4],
+	[127,3],
+	[128,4],
+	
+	[130,6],
+	[132,1],
+	# beginning of aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+	[138,2],
+	[140,3],
+	
+	[146,5],
+	[148,6],
+	[149,5],
+	[149.5,4],
+	[150,3],
+	
+	[154,1],
+	[155,2],
+	[156,2],
+	
+	[162,5],
+	[164,6],
+	[166,1],
+	[168,6],
+	[170,4],
+	[171,3],
+	[172,4],
+	[173,3],
+	[175,2],
+	[176,1],
+	
+	[179,1],
+	[180,2],
+	[181,3],
+	
+	[183,5],
+	[184,6],
+	[185,5],
+	[186,3],
+	[188,4],
+	
+	
+	
 	
 ]
 
+var bpm = 240
+var speed = 300
+var song_finished = false
+var score = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	%objPlayer.h_speed = 6
-	pass # Replace with function body.
+	%objPlayer.h_speed = 9
+	$objRhythmPreviewer.play_sample()
+	GLOBAL_MUSIC.finished.connect(on_music_end)
+	$Room_related/objPlayer.show_speed = false
+	
+	for n in notes:
+	#for note in debug_slider_starts_right_consistent:
+	#for note in debug_slider_starts_left_consistent:
+		create_note(n)
+
+func on_music_end():
+	$Room_related/objPlayer.v_speed = 400
+	$Room_related/objPlayer.h_speed = 3
+	$Room_related/objPlayer.slide = true
+	song_finished = true
+	$Instances.queue_free()
+	$Environment/tempTiles.queue_free()
 
 
+func _physics_process(_delta):
+	$Label.set_text("Score: " + str(score))
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
+func create_note(note_data):
+	var note_block = note.instantiate()
+	note_block.speed = speed
+	note_block.bpm = bpm
+	note_block.beat = note_data[0]
+	note_block.x = x_positions[note_data[1]-1]
+	note_block.collided.connect(score_points)
+	$Instances.add_child(note_block)
+	
+func score_points():
+	#if event == "note":
+		#score += 100
+	#elif event == "sliderTop" || event == "sliderBottom":
+		#score += 50
+	#elif event == "sliderBody":
+		#score += 1
+	score += 100
+	
+	#if $scoreReq && score > 5000: 
+#		$scoreReq.queue_free()
