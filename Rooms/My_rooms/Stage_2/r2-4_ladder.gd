d27801999d23b14d95cdf5286d55ecd956ff3259
Rnd-Guy <kevinngo@hotmail.co.uk>
2023-12-31 23:20:56 +0000

    add dvd logo

--- Rooms/My_rooms/Stage_2/r2-4_ladder.gd
+++ Rooms/My_rooms/Stage_2/r2-4_ladder.gd
@@ -1,15 +1,27 @@
 extends Node2D
 
+var logo_speed = 50
+var logo_velocity = Vector2(logo_speed, logo_speed)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	#$IWTLogo.position = Vector2()
+	logo_velocity[0] *= [-1,1].pick_random()
+	logo_velocity[1] *= [-1,1].pick_random()
 	pass # Replace with function body.
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-func _physics_process(_delta):
+func _physics_process(delta):
 	if is_instance_valid($Room_related/objPlayer) && $Room_related/objPlayer.position[0] < 0:
 		$Room_related/objPlayer.position[0] = 40
+	
+	$IWTLogo.position += logo_velocity * delta
+	if $IWTLogo.position[0] < 160 || $IWTLogo.position[0] > 512:
+		logo_velocity[0] *= -1
+	if $IWTLogo.position[1] < 32 || $IWTLogo.position[1] > 448:
+		logo_velocity[1] *= -1
